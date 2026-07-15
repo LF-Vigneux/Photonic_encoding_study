304004d542097bc678fdecd986d81c25cc80b316
LF-Vigneux <156103879+LF-Vigneux@users.noreply.github.com>
2026-07-15 12:11:57 -0400

    Quick ressource update

--- Dataset_complexity.sh
+++ Dataset_complexity.sh
@@ -3,7 +3,7 @@
 #SBATCH --output==Dataset_complexity_%j.out
 #SBATCH --error==Dataset_complexity_%j.err
 #SBATCH --time=13:00:00
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=4
 #SBATCH --mem=80000
 
 module load python/3.11.5
