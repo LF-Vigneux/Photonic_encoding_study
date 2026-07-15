2ae669ba8aee514538dd0c0ef8e2643809e07c76
LF-Vigneux <156103879+LF-Vigneux@users.noreply.github.com>
2026-07-15 11:52:42 -0400

    updated the memory

--- Dataset_complexity.sh
+++ Dataset_complexity.sh
@@ -3,13 +3,13 @@
 #SBATCH --output==Dataset_complexity_%j.out
 #SBATCH --error==Dataset_complexity_%j.err
 #SBATCH --time=13:00:00
-#SBATCH --cpus-per-task=30
-#SBATCH --mem=60000
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=80000
 
 module load python/3.11.5
 module load symengine/0.13.0
 
-source $HOME/venv/TEMS_qiskit_1/bin/activate
+source $HOME/venv/Encoding_Study/bin/activate
 
 cd $SLURM_SUBMIT_DIR
 
