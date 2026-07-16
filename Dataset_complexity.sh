b23c3e33787ea45c8218b1711b7f8dcbece49f88
Louis-Félix Vigneux <vigl1903@iv11.m>
2026-07-16 09:30:37 -0400

    New hardware config file

--- Dataset_complexity.sh
+++ Dataset_complexity.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=Dataset_complexity
 #SBATCH --output=Dataset_complexity_%j.out
 #SBATCH --error=Dataset_complexity_%j.err
-#SBATCH --time=13:00:00
-#SBATCH --cpus-per-task=4
+#SBATCH --time=24:00:00
+#SBATCH --cpus-per-task=6
 #SBATCH --mem=80000
 
 module load python/3.12.4
